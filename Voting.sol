b58e877aa209288439cff8704e24ebef94b86223
Emile Calixte <calixte.emile@gmail.com>
2022-10-13 11:50:51 +0200

    Add ability to register a new voter and know if an adress is a registered voter

--- Voting.sol
+++ Voting.sol
@@ -24,6 +24,11 @@ contract Voting is Ownable {
         uint voteCount;
     }
 
+    // Will take the first value (RegisteringVoters) at contract deployment
+    WorkflowStatus public workflowStatus;
+
+    mapping(address => Voter) public voters;
+
     event VoterRegistered(address VoterAddress);
 
     event WorkflowStatusChanged(WorkflowStatus previousStatus, WorkflowStatus newStatus);
@@ -31,4 +36,27 @@ contract Voting is Ownable {
     event ProposalRegistered(uint proposalId);
 
     event Voted(address voter, uint proposalId);
+
+    /**
+     * @dev Throws if called during a phase other than the voter registration phase
+     */
+    modifier onlyWhileRegisteringVoters() {
+        require(workflowStatus == WorkflowStatus.RegisteringVoters, "You can do this only during the voter registration phase");
+        _;
+    }
+
+    /**
+     * @dev Registers a new voter
+     */
+    function registerVoter(address _voterAddress) external onlyOwner onlyWhileRegisteringVoters {
+        voters[_voterAddress].isRegistered = true;
+        emit VoterRegistered(_voterAddress);
+    }
+
+    /**
+     * @dev Returns true if the address is a registered voter, false otherwise.
+     */
+    function isVoter(address _address) external view returns (bool) {
+        return voters[_address].isRegistered;
+    }
 }
