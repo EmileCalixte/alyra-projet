d52d5e9ad390634da81f23debd8c953a7c8f0c6b
Emile Calixte <calixte.emile@gmail.com>
2022-10-13 11:10:35 +0200

    Create Voting contract structure

---
+++ Voting.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Voting is Ownable {
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint votedProposalId;
+    }
+
+    struct Proposal {
+        string description;
+        uint voteCount;
+    }
+
+    event VoterRegistered(address VoterAddress);
+
+    event WorkflowStatusChanged(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+
+    event ProposalRegistered(uint proposalId);
+
+    event Voted(address voter, uint proposalId);
+}
