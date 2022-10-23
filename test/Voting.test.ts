3964a018c112093ce2a9b43a63fe20cb608d9d4f
Emile Calixte <calixte.emile@gmail.com>
2022-10-23 19:13:40 +0200

    Add tests for RegisterVoter function

--- test/Voting.test.ts
+++ test/Voting.test.ts
@@ -24,7 +24,7 @@ describe("Voting", () => {
             expect(await voting.isVoter(otherAccounts[0].address)).to.equal(true);
         });
 
-        it("Should prevent other accounts to register a voter", async () => {
+        it("Should prevent other account to register a voter", async () => {
             const { voting, otherAccounts } = await loadFixture(deployFixture);
 
             expect(await voting.isVoter(otherAccounts[0].address)).to.equal(false);
@@ -35,12 +35,35 @@ describe("Voting", () => {
             expect(await voting.isVoter(otherAccounts[0].address)).to.equal(false);
         });
 
+        it("Should prevent other account to register a voter even if it's a registered voter", async () => {
+            const { voting, otherAccounts } = await loadFixture(deployFixture);
+
+            await voting.registerVoter(otherAccounts[0].address);
+
+            await expect(voting.connect(otherAccounts[0]).registerVoter(otherAccounts[1].address))
+                .to.be.revertedWith("Ownable: caller is not the owner");
+
+            expect(await voting.isVoter(otherAccounts[1].address)).to.equal(false);
+        });
+
         it("Should emit VoterRegistered event", async () => {
             const { voting, otherAccounts } = await loadFixture(deployFixture);
 
             await expect(voting.registerVoter(otherAccounts[0].address))
                 .to.emit(voting, "VoterRegistered")
                 .withArgs(otherAccounts[0].address);
+        });
+
+        it("Should prevent to register a voter if Registering Voters phase is closed", async () => {
+            const { voting, otherAccounts } = await loadFixture(deployFixture);
+
+            // End registering voters phase by starting the next phase
+            await voting.startProposalsRegistration();
+
+            await expect(voting.registerVoter(otherAccounts[0].address))
+                .to.be.revertedWith("You can do this only during the voter registration phase");
+
+            expect(await voting.isVoter(otherAccounts[0].address)).to.equal(false);
         });
     });
 });
