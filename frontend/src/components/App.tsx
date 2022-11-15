6849937e4174c6fa0f4e68452f03b1a43c4c7d34
Emile Calixte <calixte.emile@gmail.com>
2022-11-15 11:18:47 +0100

    Add ability to connect to Metamask

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -1,9 +1,36 @@
 import '../css/index.css';
+import {ethers} from "ethers";
+import {useState} from "react";
 
 const App = () => {
+    const [account, setAccount] = useState<string|undefined>(undefined);
+
+    const connectToMetamask = async () => {
+        if (!window.ethereum) {
+            console.error("No provider found in navigator");
+            return;
+        }
+
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+        console.log('provider', provider);
+
+        const accounts = await provider.send("eth_requestAccounts", []);
+
+        console.log(accounts);
+
+        setAccount(accounts[0]);
+    }
+
     return (
         <div className="app">
-            Hello world
+            {account === undefined &&
+            <button onClick={connectToMetamask}>Connect to Metamask</button>
+            }
+
+            {account !== undefined &&
+            <p>Welcome {account}</p>
+            }
         </div>
     );
 }
