49ce3127ef64e0933ae0254e49e799bdec2bba7f
Emile Calixte <calixte.emile@gmail.com>
2022-11-15 18:33:49 +0100

    Compute winning proposal ID at each vote and remove tally votes loop

--- hardhat/contracts/Voting.sol
+++ hardhat/contracts/Voting.sol
@@ -91,6 +91,10 @@ contract Voting is Ownable {
         voters[msg.sender].hasVoted = true;
         proposalsArray[_id].voteCount++;
 
+        if (proposalsArray[_id].voteCount > proposalsArray[winningProposalID].voteCount) {
+            winningProposalID = _id;
+        }
+
         emit Voted(msg.sender, _id);
     }
 
@@ -129,15 +133,9 @@ contract Voting is Ownable {
 
    function tallyVotes() external onlyOwner {
        require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
-       uint _winningProposalId;
-      for (uint256 p = 0; p < proposalsArray.length; p++) {
-           if (proposalsArray[p].voteCount > proposalsArray[_winningProposalId].voteCount) {
-               _winningProposalId = p;
-          }
-       }
-       winningProposalID = _winningProposalId;
-       
+
        workflowStatus = WorkflowStatus.VotesTallied;
+
        emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
     }
 }
