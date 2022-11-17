0242951de1ae3ceda03c96250b6d4706f3e062cb
Emile Calixte <calixte.emile@gmail.com>
2022-11-17 16:34:19 +0100

    Install react-router-dom package

--- frontend/src/components/VotingInterface.tsx
+++ frontend/src/components/VotingInterface.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "./layout/Header";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 
 const VotingInterface: React.FunctionComponent = () => {
     return (
         <div className="voting-app">
             <Header />
 
-            TODO
+            <div className="container-fluid">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={"Home"} />
+
+                        {/* Redirect any unresolved route to home */}
+                        <Route path="*" element={<Navigate to="/" replace/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </div>
         </div>
     )
 }
