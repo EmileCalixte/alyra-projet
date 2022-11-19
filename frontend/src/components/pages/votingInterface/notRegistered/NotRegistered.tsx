8c82145630828a4e478c9e4ac092a5e171df6bc1
Emile Calixte <calixte.emile@gmail.com>
2022-11-19 15:08:24 +0100

    Remove useless divs in NotRegistered component

--- frontend/src/components/pages/votingInterface/notRegistered/NotRegistered.tsx
+++ frontend/src/components/pages/votingInterface/notRegistered/NotRegistered.tsx
@@ -1,17 +1,11 @@
 const NotRegistered = () => {
     return (
-        <div className="page-not-registered-error">
+        <div className="page-content">
             <div className="row">
                 <div className="col-12">
-                    <div className="page-content">
-                        <div className="row">
-                            <div className="col-12">
-                                <p style={{color: "#ff0000"}}>
-                                    You cannot use this application because you are not a registered voter.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <p style={{color: "#ff0000"}}>
+                        You cannot use this application because you are not a registered voter.
+                    </p>
                 </div>
             </div>
         </div>
